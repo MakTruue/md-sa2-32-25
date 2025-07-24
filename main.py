98847a9a05b093593d0b968ced411b8780d8cbe4
Ruslan Makarevich <Makarevich.True@gmail.com>
2025-07-24 02:44:52 +0000

    Add test PY_2

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ def to_upper(text):
     return text.upper()
 
 if __name__ == "__main__":
-    user_input = input("ruslan makarevich")
+    user_input = "ruslan makarevich"
     print("В верхнем регистре:", to_upper(user_input))
